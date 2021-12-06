05038a5247dcca8380586f8d72aaf33a616bb622
nthomson0317 <nicholasgraythomson@gmail.com>
2021-12-06 17:27:46 -0500

    dog class

--- OOP.py
+++ OOP.py
@@ -1,1 +1,14 @@
 # python-oop
+
+class Dog():
+    
+    def __init__(self,breed):
+        self.breed = breed
+
+my_dog = Dog('boxer')
+
+print(type(my_dog))
+#<class '__main__.Dog'>
+
+print(my_dog.breed)
+#==> boxer
