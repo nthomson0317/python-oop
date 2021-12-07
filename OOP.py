ec36df9140bfc8d4d9adaf31659547ce38e5b14f
nthomson0317 <nicholasgraythomson@gmail.com>
2021-12-07 18:39:55 -0500

    species

--- OOP.py
+++ OOP.py
@@ -9,12 +9,20 @@ class Dog():
         #Assign it using self.attribute_name
         self.breed = breed
         self.name = name
+
+        # Expect boolean
         self.spots = spots
 
-my_dog = Dog('boxer')
+my_dog = Dog(breed='boxer',name='Basil',spots=False)
 
 print(type(my_dog))
 #<class '__main__.Dog'>
 
 print(my_dog.breed)
 #==> boxer
+
+print(my_dog.name)
+#==> Basil
+
+print(my_dog.spots)
+#==> False
