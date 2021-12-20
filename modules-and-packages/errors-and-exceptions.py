fed57058f764f67518191dd4cdea574d55c92541
nthomson0317 <nicholasgraythomson@gmail.com>
2021-12-20 17:17:06 -0500

    try except

--- modules-and-packages/errors-and-exceptions.py
+++ modules-and-packages/errors-and-exceptions.py
@@ -18,3 +18,22 @@ number2 = input("Please provide a number")
 
 add(number1,number2)
 #==> TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+try:
+    #WANT TO ATTEMPT THIS CODE
+    #MAY HAVE AN ERROR
+    result = 10 + 10
+except: 
+    print("Hey it looks like you aren't adding correctly.")
+
+result 
+#==> 20
+
+try:
+    #WANT TO ATTEMPT THIS CODE
+    #MAY HAVE AN ERROR
+    result = 10 + '10'
+except: 
+    print("Hey it looks like you aren't adding correctly.")
+    #==> Hey it looks like you aren't adding correctly.
+
