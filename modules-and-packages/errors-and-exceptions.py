d1e0019f2f7c89afd9676207f7d23959154b136a
nthomson0317 <nicholasgraythomson@gmail.com>
2021-12-20 17:25:21 -0500

    ask for int

--- modules-and-packages/errors-and-exceptions.py
+++ modules-and-packages/errors-and-exceptions.py
@@ -53,3 +53,10 @@ except OSError:
 finally:
     print("I always run")
 
+def ask_for_int():
+    try:
+        result = int(input("Please provide number"))
+    except:
+        print("Whoops! That is not a number")
+    finally:
+        print("End of try/except/finally")
