ab99fe02d041c1923a4805a273e2544d26e3acfc
nthomson0317 <nicholasgraythomson@gmail.com>
2021-12-21 09:25:43 -0500

    zero division error

--- modules-and-packages/errors-hw.py
+++ modules-and-packages/errors-hw.py
@@ -15,7 +15,24 @@ y = 0
 
 try:
     z = x/y
-except:
+except ZeroDivisionError:
     print("Whoops! You can't divide by zero!")
 finally:
     print("ALl done")
+
+#Problem 3
+#Write a function that asks for an integer and prints the square of it. Use a while loop with a try, except, else block to account for incorrect inputs.
+
+def ask():
+    while True:
+        try:
+            result = int(input("Please provide number"))
+            
+        except:
+            print("An error occurred! Please try again")
+        else:
+            print(f"Thank you, your number squared is {result ** 2}")
+            break
+
+ask()
+        
