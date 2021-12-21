4ed742f926907ccc9f63241703f3d5f4ade8cf01
nthomson0317 <nicholasgraythomson@gmail.com>
2021-12-21 09:18:08 -0500

    problem 2

--- modules-and-packages/errors-hw.py
+++ modules-and-packages/errors-hw.py
@@ -1,6 +1,21 @@
 #Errors and Exceptions Homework
 #Problem 1
 #Handle the exception thrown by the code below by using try and except blocks.
+try:
+    for i in ['a','b','c']:
+        print(i**2)
+except:
+    print('Whoops! You can only multiply numbers')
 
-for i in ['a','b','c']:
-    print(i**2)
+##Problem 2
+#Handle the exception thrown by the code below by using try and except blocks. Then use a finally block to print 'All Done.'
+
+x = 5
+y = 0
+
+try:
+    z = x/y
+except:
+    print("Whoops! You can't divide by zero!")
+finally:
+    print("ALl done")
