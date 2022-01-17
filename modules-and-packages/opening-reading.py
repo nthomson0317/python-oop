f1a6397fcbcc6f011b18920ead6334007c2635e7
nthomson0317 <nicholasgraythomson@gmail.com>
2022-01-17 17:10:14 -0500

    move file

--- modules-and-packages/opening-reading.py
+++ modules-and-packages/opening-reading.py
@@ -7,3 +7,13 @@ import os
 
 print(os.getcwd())
 #/Users/nicholas.thomson/Desktop/Development/python/modules-and-packages
+
+print(os.listdir())
+#['collections-module.py', 'errors-and-exceptions.py', 'pip-pypi.py', 'opening-reading.py', 'setdefault.py', 'practice.txt', 'errors-hw.py']
+
+import shutil
+
+# shutil.move('practice.txt','/Users/nicholas.thomson')
+
+os.listdir('/Users/nicholas.thomson')
+#['collections-module.py', 'errors-and-exceptions.py', 'pip-pypi.py', 'opening-reading.py', 'setdefault.py', 'practice.txt', 'errors-hw.py']
