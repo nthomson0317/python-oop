536e4c62f0690e668d9bccbf4b5467b40ac12f14
nthomson0317 <nicholasgraythomson@gmail.com>
2022-01-17 17:06:44 -0500

    getcwd

--- modules-and-packages/opening-reading.py
+++ modules-and-packages/opening-reading.py
@@ -1,3 +1,9 @@
 f = open('practice.txt','w+')
 f.write('This is a test string')
 f.close()
+
+
+import os
+
+print(os.getcwd())
+#/Users/nicholas.thomson/Desktop/Development/python/modules-and-packages
