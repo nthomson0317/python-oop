e632b73ac183a86178f90fe409e70d9e33a0a986
nthomson0317 <nicholasgraythomson@gmail.com>
2022-01-31 11:19:12 -0500

    append linked list

--- dsa/linked-list.py
+++ dsa/linked-list.py
@@ -2,3 +2,16 @@ class node:
     def __init__(self,data=None):
         self.data = data
         self.next = None
+
+class linked_list:
+    def __init__(self):
+        self.head = node()
+    
+    def append(self,data):
+        new_node = node(data)
+        curr = self.head
+        while curr.next!=None:
+            curr = curr.next
+        curr.next = new_node
+    
+    
