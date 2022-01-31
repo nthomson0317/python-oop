556a9be2d001165cf5093fc3aa53ce9356a1e462
nthomson0317 <nicholasgraythomson@gmail.com>
2022-01-31 17:27:08 -0500

    big o

---
+++ dsa/big-o.py
@@ -0,0 +1,38 @@
+def print_items(n):
+    for i in range(n):
+        print(i)
+
+print_items(10)
+
+#O(n) --> it ran n times
+
+##Drop Constants
+    ## n+n
+        ##still #O(n)
+
+def print_items(n):
+    for i in range(n):
+        print(i)
+    for j in range(n):
+        print(j)
+
+print_items(10)
+
+
+##O(n^2)
+    ## n*n
+
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            print(i,j)
+print_items(10)
+
+    ## n*n*n
+        ##still O(n^2)
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                print(i,j,k)
+print_items(10)
